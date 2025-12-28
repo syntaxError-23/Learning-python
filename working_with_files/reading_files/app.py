f47ccf6a1b749fdb4cc25f4e612f440133cb95ed
Josef K <ahmed.abshir91@gmail.com>
2025-12-28 03:57:28 +0000

    restructures directories and adds reading files directory

---
+++ working_with_files/reading_files/app.py
@@ -0,0 +1,28 @@
+
+
+def read_file():
+    #open file
+    file = open("authors.txt", "r")
+
+    #read file
+    #content = file.read()
+    #print(content)
+
+    #return a list (iterable) of lines from the file
+    lines = file.readlines()
+
+    for line in lines:
+        print(line)
+        print("i'm working")
+
+    #close file
+    file.close()
+
+    return
+
+def main():
+    read_file()
+    return
+
+if __name__ == "__main__":
+    main()
