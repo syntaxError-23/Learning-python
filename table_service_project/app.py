74f474c96dd181f1b79a136f46c3af9d6a984e8b
Josef K <ahmed.abshir91@gmail.com>
2025-12-31 08:36:30 +0000

    Adds directory and app file for table service project

---
+++ table_service_project/app.py
@@ -0,0 +1,19 @@
+
+
+
+
+def main():
+
+    #get table number from user
+
+    #create file path using table number
+
+    #get items from user (item name and price)
+
+    #calculate totals (total tip grand total)
+
+    #create the csv 
+    return
+
+if __name__ == "__main__":
+    main()
